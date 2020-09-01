b440c51ddfad56204fe59197b0873efffc775a47
ADITYA150106 <67780629+ADITYA150106@users.noreply.github.com>
2020-09-01 21:01:04 +0530

    Calculator code

---
+++ SIMPLE_CALCULATOR.py
@@ -0,0 +1,51 @@
+#simple calculator
+
+def addition(num1,num2) :
+		result = num1 + num2
+		print(' {0} + {1} = {2}' .format(num1,num2,result))				
+			
+def subtraction(num1,num2) :
+		result = num1 - num2
+		print(' {0} - {1} = {2}' .format(num1,num2,result))
+	
+def multiplication(num1,num2) :
+		result = num1 * num2
+		print(' {0} * {1} = {2}' .format(num1,num2,result))
+		
+def division(num1,num2) :
+		if num2 == 0.0 :
+			print('cant do divide by zero')
+		else :
+			    result = num1 / num2
+			    print(' {0} / {1} = {2}' .format(num1,num2,result))
+
+# putting in loop 
+
+while True :
+	print('What do you want to do ?')
+	print('1.Addition')
+	print('2.Subtraction')
+	print('3.Multiplication')
+	print('4.Division')
+	print('Enter Q or q to Exit')
+
+	choice = input('Enter your choice : ')
+	
+	if choice == 'Q' or choice == 'q' :
+		break
+	
+	num1 = float(input("Enter Number 1 : "))
+	num2 = float(input("Enter Number 2 : "))
+
+	if choice == '1' :
+		addition(num1,num2)
+	elif choice == '2' :
+		subtraction(num1,num2)
+	elif choice == '3' :
+		multiplication(num1,num2)
+	elif choice == '4' :
+		division(num1,num2)
+	else :
+		print('Invalid Choice')
+		
+	print('\n')
